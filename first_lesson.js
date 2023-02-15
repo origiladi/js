d198fc8d65f34f763026eea0b9b1563e0eedac85
origiladi <120328585+origiladi@users.noreply.github.com>
2023-02-15 18:04:09 +0200

    Add files via upload

---
+++ first_lesson.js
@@ -0,0 +1,96 @@
+//ex 1
+/* var n1 =3;
+var n2 =4;
+n1 === n2? console.log('boom') :  console.log('t'); */
+//ex 2
+/* var n1 =3;
+var n2 =4;
+console.log(n1 + n2) */
+//ex 3
+/* var n1 =3;
+var n2 =4;
+console.log(n1 * n2) */
+//ex 4
+/* var n =4;
+if(n===1)
+console.log('gw')
+else if(n>1 && n<5)
+console.log('gd')
+else
+consle.log('h') */
+//ex 5
+/*  var n =4;
+ if(n===1)
+ console.log('gm')
+ else if(n>1 && n<11)
+ console.log('sw')
+ else if(n===10)
+ consle.log('gs') 
+ else if(n>10 && n<21)
+ consle.log('bh')
+ else if(n>20 && n<31)
+ consle.log('th') 
+ else
+   consle.log('bb') */
+   //ex 6
+/*     var n ='purim';
+    if(n ==='purim')
+    console.log('purim')
+    else if(n ==='passover')
+    console.log('clean')
+    else if(n ==='shavuor')
+    console.log('milk')
+    else
+      console.log('bs') */
+ //ex 7
+/*  var p1='s';
+ var p1s = 60;
+  var p2='a';
+ var p2s = 30;
+ p1s> p2s ? console.log(p1) : console.log(p2) */
+ //ex 8
+/*  var n =7;
+ n % 2 === 0 ?  console.log('yes') : console.log('no') */  
+ //ex 9
+/*   var n =7;
+   var n2 =71;
+   n > n2? console.log(n) : console.log(n2) */
+ // ex 10
+/*   var n =7;
+   var n2 =7;
+    var n3 =7;
+     var n4 =7;
+      var n5 =7;
+       var n6 =7;
+       var n7 =7;
+     console.log((n + n2+n3+n4+n5+n6+n7)/7) */
+     // ex 11
+/*      var name ='ori'
+     var age = 30
+     var km = 4
+     if(age>29 && age <50)
+     {
+         if(km >30)
+          console.log('great')
+         else if(km >10)
+          console.log('good')
+          else
+          console.log('bad')
+     }
+      if(age>17 && age <30)
+      {
+           if(km >25)
+          console.log('great')
+      
+          else
+          console.log('bad')
+      } */
+      //ex 12
+  /*     var km =23
+      
+         if(km >5 && km <12)
+          console.log('gm')
+         else if(km >11 && km <18)
+          console.log('ga')
+          else if (km >17 && km <24)
+          console.log('ge') */
